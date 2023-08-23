7bcf6b06c135a69aaf690f59272082b911b39f7a
Megan Moore <meganmoore.dev@gmail.com>
2023-08-23 15:28:40 -0400

    Add project files.

---
+++ TestExtensionLibrary/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExtensionLibrary;
+
+
+var s = "       $12,242.78";
+var s1 = "000000000008741452";
+s1.Writeline("The value of s1 is ", " pretty cool!");
+
+/*
+
+var s = "asdHSkl";
+
+var sAllCaps = s.AllCaps();
+var sAllSmall = sAllCaps.AllSmall();
+
+Console.WriteLine($" {s} as all caps is {sAllCaps} and small is {sAllSmall}");
+
+*/
